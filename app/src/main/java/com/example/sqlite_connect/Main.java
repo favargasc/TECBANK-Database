d224a7089c0f3955b1ce3c1a7d59788dc4f7027b
favargasc <75912618+favargasc@users.noreply.github.com>
2022-03-13 22:45:54 -0600

    Se agregó un ejemplo de actualización de datos para los sobres de ahorro

--- app/src/main/java/com/example/sqlite_connect/Main.java
+++ app/src/main/java/com/example/sqlite_connect/Main.java
@@ -1,17 +1,15 @@
 package com.example.sqlite_connect;
 
-import com.example.sqlite_connect.controllers.LocationController;
-import com.example.sqlite_connect.models.Location;
+import com.example.sqlite_connect.controllers.SavingEnvelopeController;
+import com.example.sqlite_connect.models.SavingEnvelope;
+
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        LocationController locationController = new LocationController();
-        ArrayList<Location> locations = locationController.getLocations();
-
-        for (int i = 0; i < locations.size(); i++) {
-            System.out.println(locations.get(i).getCoordinates());
-        }
-
+        SavingEnvelopeController seController = new SavingEnvelopeController();
+        seController.receiveMoney(4, 1000.0);
     }
 }
+
--- app/src/main/java/com/example/sqlite_connect/controllers/SavingEnvelopeController.java
+++ app/src/main/java/com/example/sqlite_connect/controllers/SavingEnvelopeController.java
@@ -4,6 +4,7 @@ import com.example.sqlite_connect.config.DBConnection;
 import com.example.sqlite_connect.models.SavingEnvelope;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -34,4 +35,34 @@ public class SavingEnvelopeController {
         }
         return SavingEnvelopes;
     }
+
+    public void receiveMoney(Integer saving_envelope_id, Double amount) {
+        String updateSE =   "UPDATE saving_envelope SET current_balance = se.current_balance + ? FROM saving_envelope se INNER JOIN account a on a.id = se.account_id WHERE se.id = ? AND a.current_balance >= ?";
+        String updateA  =   "UPDATE account SET current_balance = a.current_balance - ? FROM saving_envelope se INNER JOIN account a on a.id = se.account_id WHERE se.id = ? AND a.current_balance >= ?";
+
+       try {
+            DBConnection dbConnection = new DBConnection();
+            Connection connection = dbConnection.getConnection();
+
+           PreparedStatement statementUpdateSE = connection.prepareStatement(updateSE);
+           statementUpdateSE.setDouble(1, amount);
+           statementUpdateSE.setInt(2, saving_envelope_id);
+           statementUpdateSE.setDouble(3, amount);
+           statementUpdateSE.executeUpdate();
+
+           PreparedStatement statementUpdateA = connection.prepareStatement(updateA);
+           statementUpdateA.setDouble(1, amount);
+           statementUpdateA.setInt(2, saving_envelope_id);
+           statementUpdateA.setDouble(3, amount);
+           statementUpdateA.executeUpdate();
+
+            connection.close();
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+    }
+
+
 }
+
+
