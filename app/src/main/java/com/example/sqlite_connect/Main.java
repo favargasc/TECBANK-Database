2df3af47301ab8c33f819f7a572418930f87d0ca
favargasc <75912618+favargasc@users.noreply.github.com>
2022-03-14 16:43:17 -0600

    Se Finalizó el módulo de autenticación por token via email

--- app/src/main/java/com/example/sqlite_connect/Main.java
+++ app/src/main/java/com/example/sqlite_connect/Main.java
@@ -1,10 +1,23 @@
 package com.example.sqlite_connect;
 
 import com.example.sqlite_connect.controllers.SavingEnvelopeController;
+import org.apache.commons.lang3.RandomStringUtils;
+import com.example.sqlite_connect.utilities.Email;
 
 public class Main {
     public static void main(String[] args) {
-        SavingEnvelopeController seController = new SavingEnvelopeController();
-        seController.returnMoney(4, 1000);
+        //SavingEnvelopeController seController = new SavingEnvelopeController();
+
+        // ANTES DE METER LOS DATOS HABILITEN LA AUTH
+        //https://myaccount.google.com/lesssecureapps
+        //https://accounts.google.com/b/0/DisplayUnlockCaptcha
+        Email email = new Email(
+            "",
+            "",
+            "",
+            "TECBANK Código de autenticación",
+            RandomStringUtils.randomAlphanumeric(20)
+        );
+        email.send();
     }
 }
