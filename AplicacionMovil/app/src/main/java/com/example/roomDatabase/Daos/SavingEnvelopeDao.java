6a5e26c3d99c9593aab2cb4f3d85ed74ab8095be
favargasc <75912618+favargasc@users.noreply.github.com>
2022-03-25 05:56:40 -0600

    actualización SE Dao

--- AplicacionMovil/app/src/main/java/com/example/roomDatabase/Daos/SavingEnvelopeDao.java
+++ AplicacionMovil/app/src/main/java/com/example/roomDatabase/Daos/SavingEnvelopeDao.java
@@ -5,7 +5,7 @@ import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 
-import com.example.roomDatabase.models.Bank;
+import com.exmaple.roomDatabase.models.Account;
 import com.example.roomDatabase.models.SavingEnvelope;
 
 import java.util.List;
@@ -23,23 +23,23 @@ public interface SavingEnvelopeDao {
   void receiveMoney(int savingEnvelopeId, double amount);
 
   @Query("UPDATE saving_envelope SET current_balance = current_balance - :amount WHERE id = :savingEnvelopeId")
-  LiveData<Boolean> returnMoney(int savingEnvelopeId, double amount);
+  void returnMoney(int savingEnvelopeId, double amount);
 
   @Query("DELETE FROM saving_envelope WHERE id = :savingEnvelopeId")
-  LiveData<Boolean> delete(int savingEnvelopeId);
+  void delete(int savingEnvelopeId);
 
   @Query("SELECT a.id, a.account_number, a.current_balance FROM saving_envelope se INNER JOIN account a ON a.id = se.account_id WHERE se.id = :savingEnvelopeId")
-  LiveData<Boolean> getAccount(int savingEnvelopeId);
+  LiveData<Account> getAccount(int savingEnvelopeId);
 
   @Query("select id, name, current_balance from saving_envelope where id = :savingEnvelopeId")
-  LiveData<Boolean> getSavingEnvelope(int savingEnvelopeId);
+  LiveData<SavingEnvelope> getSavingEnvelope(int savingEnvelopeId);
 
   @Query("UPDATE account SET current_balance = current_balance - :amount WHERE id = :savingEnvelopeId")
-  LiveData<Boolean> reduceAccountCB(int savingEnvelopeId, double amount);
+  void reduceAccountCB(int savingEnvelopeId, double amount);
 
   @Query("UPDATE account SET current_balance = current_balance + :amount WHERE id = :savingEnvelopeId")
-  LiveData<Boolean> increaseAccountCB(int savingEnvelopeId, double amount);
+  void increaseAccountCB(int savingEnvelopeId, double amount);
 
   @Query("INSERT INTO saving_envelope_log (amount, date, transaction_type_id) VALUES (:amount, :date, :transactionTypeId)")
-  LiveData<Boolean> saveInLog(double amount, String date, int transactionTypeId);
+  void saveInLog(double amount, String date, int transactionTypeId);
 }
